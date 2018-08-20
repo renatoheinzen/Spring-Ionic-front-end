f6a16283a48fa0f5ff8af800637ec29663952aed
Renato Heinzen <renato.heinzen@outlook.com>
2018-08-20 19:16:04 -0300

    Aproveitando os dados do usuário logado na tela inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -24,6 +24,15 @@ export class AuthService{
                             responseType: 'text'
                         });
     }
+    refreshToken(){
+        return this.http.post(
+                    `${API_CONFIG.baseUrl}/auth/refresh_token`,
+                       {},
+                        {
+                            observe: 'response', 
+                            responseType: 'text'
+                        });
+    }
 
     successfullLogin(authorizationValue: string) {
         console.log(authorizationValue);
